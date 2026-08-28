3ac591036dd5c9d49b0b1283ad2a7f8877db3846
Bruno Malta Barros <bruno.barros@al.sebrae.com.br>
2026-08-28 08:33:09 -0300

    criação do arquivo das exceções de domínio (por enquanto apenas as estruturas das classes)

---
+++ app/exceptions/exceptions.py
@@ -0,0 +1,10 @@
+class AppException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+class NotFoundException(AppException):
+    pass
+
+class ConflictException(AppException):
+    pass
